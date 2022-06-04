a40a67c731e51f235cea8f6cac810379ab114acb
Mikołaj Radek <radek_mikolaj@o2.pl>
2022-06-05 01:26:18 +0200

    static calculator

---
+++ components/index.js
@@ -0,0 +1,26 @@
+const resultBasic = document.querySelector('.result')
+
+const one = document.querySelector('.one_calc')
+const two = document.querySelector('.two_calc')
+const three = document.querySelector('.three_calc')
+const  four = document.querySelector('.four_calc')
+const five = document.querySelector('.five_calc')
+const six = document.querySelector('.six_calc')
+const seven = document.querySelector('.seven_calc')
+const eight = document.querySelector('.eight_calc')
+const nine = document.querySelector('.nine_calc')
+const zero = document.querySelector('.zero_calc')
+
+
+const c = document.querySelector('.delete_c')
+const delete_left = document.querySelector('.fa-delete-left')
+const multiply = document.querySelector('.multipy_calc')
+const minus = document.querySelector('.minus_calc')
+const plus = document.querySelector('.plus_calc')
+const equals = document.querySelector('.equals_calc')
+const comma = document.querySelector('.comma_calc')
+
+
+resultBasic.innerHTML = "5"
+
+
